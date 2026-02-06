8a711a90665d6a0c5ba5309ab49b5bbfd2255617
Moises Franco Gtz <moisesfrancogtz@gmail.com>
2026-02-05 23:27:25 -0600

    [add] : feature task

--- backend-api/src/app.module.ts
+++ backend-api/src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432'),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: process.env.NODE_ENV !== 'production',
+      logging: process.env.DB_LOGGING === 'true',
+    }),
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
